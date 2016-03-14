6bcc87a7b507abc50cbf3a18a07ce76793c2d3d8
Alpana30 <alpana.a.chaphalkar@outlook.com>
2016-03-14 04:22:07 +0100

    Added files via upload

--- JS/tweetsData.js
+++ JS/tweetsData.js
@@ -3,14 +3,43 @@ var div = d3.select('body')
                 .attr("class", "tip")
                 .style("opacity", 0);	
 
-var id_name = ' ';
-var data_value = [];
-  
-d3.select('#tweets')
-        .data([10])
-        .on('mouseover', function (d) {	
+var theData = [
+    {
+        id_name: '#tweets',
+        text_value: 'Tweets',
+        data_value: 10
+    },
+    {
+        id_name: '#tags',
+        text_value: 'Tags',
+        data_value: 20
+    },
+    {
+        id_name: '#users',
+        text_value: 'Users',
+        data_value: 30
+    },
+    {
+        id_name: '#langs',
+        text_value: 'Languages',
+        data_value: 40
+    },
+]
+
+d3.select('nav')
+       .selectAll('a')
+       .data(theData)
+       .enter()
+       .append('a')
+       .attr('id',function (d) {
+           return d.id_name;
+       })
+       .text(function (d) {
+           return d.text_value;
+       })
+       .on('mouseover', function (d) {	
              div.style('opacity', 1);   
-             div.html("Total: " + d)
+             div.html("Total: " + d.data_value)
                 .style("position", "relative")	
                 .style("left", (d3.event.pageX) + "px")		
                 .style("top", (d3.event.pageY) + "px");	
@@ -18,42 +47,3 @@ d3.select('#tweets')
         .on("mouseout", function(d) {		
             div.style("opacity", 0);	
         });
-        
-d3.select('#tags')
-    .data([20])
-    .on('mouseover', function (d) {	
-             div.style('opacity', 1);   
-             div.html("Total: " + d)
-                .style("position", "relative")	
-                .style("left", (d3.event.pageX) + "px")		
-                .style("top", (d3.event.pageY) + "px");	
-    })
-    .on("mouseout", function(d) {		
-            div.style("opacity", 0);	
-     });
-     
-d3.select('#users')
-    .data([30])
-    .on('mouseover', function (d) {	
-             div.style('opacity', 1);   
-             div.html("Total: " + d)
-                .style("position", "relative")	
-                .style("left", (d3.event.pageX) + "px")		
-                .style("top", (d3.event.pageY) + "px");	
-    })
-    .on("mouseout", function(d) {		
-            div.style("opacity", 0);	
-    });
-
-d3.select('#langs')
-    .data([40])
-    .on('mouseover', function (d) {	
-             div.style('opacity', 1);   
-             div.html("Total: " + d)
-                .style("position", "relative")	
-                .style("left", (d3.event.pageX) + "px")		
-                .style("top", (d3.event.pageY) + "px");	
-    })
-    .on("mouseout", function(d) {		
-            div.style("opacity", 0);	
-    });
